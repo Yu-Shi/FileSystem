2d3d131974b1bf9f6b474f5b3b11b54d2b9ebe44
Shi <dgdsxyushi@126.com>
2017-12-08 14:58:06 +0800

    first commit

---
+++ operations.h
@@ -0,0 +1,60 @@
+//
+// Created by Shi on 2017/12/8.
+//
+
+#ifndef FILESYSTEM_OPERATIONS_H
+#define FILESYSTEM_OPERATIONS_H
+
+#endif //FILESYSTEM_OPERATIONS_H
+
+#include <string>
+#include <cstring>
+using namespace std;
+
+//pwd
+void getLocation()
+{
+
+}
+
+//cd $path
+int setLocation(string path)
+{
+
+}
+
+//mkdir $path
+int newItem(string path)
+{
+
+}
+
+//ls $path
+int getChildItem(string path)
+{
+
+}
+
+//rmdir $path
+int removeDirectory(string path)
+{
+
+}
+
+//echo $str $path
+int writeOutput(string str, string path)
+{
+
+}
+
+//cat $path
+int getContent(string path)
+{
+
+}
+
+//rm $path
+int removeFile(string path)
+{
+
+}
---
+++ structs.h
@@ -0,0 +1,39 @@
+//
+// Created by Shi on 2017/12/8.
+//
+
+#ifndef FILESYSTEM_STRUCTS_H
+#define FILESYSTEM_STRUCTS_H
+
+#endif //FILESYSTEM_STRUCTS_H
+
+struct super_block
+{
+    bool inode_bitmap[4096];
+    bool block_bitmap[4096];
+};
+
+struct inode
+{
+    int i_id;
+    int i_mode;
+    int i_file_size;
+    int i_blocks[1];
+    char i_place_holder[16];
+};
+
+struct file_block
+{
+    char data[4096];
+};
+
+struct dir_entry
+{
+    char name[252];
+    int inode_id;
+};
+
+struct dir_block
+{
+    dir_entry dirs[16];
+};
